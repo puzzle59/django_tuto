f777837eec477a1bca0a0572e54f44fd49b7aafa
puzzle59 <cimettamarvin@gmail.com>
2020-06-19 01:17:53 +0200

    je m'y attèle

--- personnal_portfolio-project/blog/urls.py
+++ personnal_portfolio-project/blog/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
 
 
     path('', views.all_blogs,name="all_blogs"),
+    path('<int:blog_id>/', views.detail,name="detail"),
 
 ]
--- personnal_portfolio-project/blog/views.py
+++ personnal_portfolio-project/blog/views.py
@@ -1,13 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from .models import Blog
 def all_blogs(request):
     blogs=Blog.objects.all()
     return render(request,'blog/all_blogs.html', {'blogs':blogs})
 
-
-
-
-
-
-    projects=Project.objects.all()
-    return render(request,'portfolio/home.html',{'projects':projects})
+def detail(request, blog_id):
+    return render(request, 'blog/detail.html', {'id':blog_id})
